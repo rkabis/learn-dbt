06f14c0c2af5447b86b4e7627ada2bf543a7580f
Ryan Abis <rkabis@up.edu.ph>
2021-09-10 14:51:55 +0000

    ref: placed dollar conversion as macro

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -3,7 +3,7 @@ with payments as (
   select
     id as payment_id,
     orderid as order_id,
-    amount / 100 as amount,
+    {{ cents_to_dollar('amount') }} as amount,
     created as created_at,
     paymentmethod as payment_method,
     status
